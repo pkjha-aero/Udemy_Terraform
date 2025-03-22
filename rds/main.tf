faf86a716e4887ffc4edab3c998f0542eb2112f9
Pankaj Jha <jha3@llnl.gov>
2025-03-21 18:04:49 -0700

    Update the terraform file for creating an RDS

--- rds/main.tf
+++ rds/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    region = "eu-west-2"
+    region = "us-west-1"
 }
 
 resource "aws_db_instance" "myRDS" {
     db_name = "myDB"
     identifier = "my-first-rds"
-    instance_class = "db.t2.micro"
+    instance_class = "db.m7g.large"
     engine = "mariadb"
-    engine_version = "10.2.21"
+    engine_version = "10.4.29"
     username = "bob"
     password = "password123"
     port = 3306
